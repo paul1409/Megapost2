8f04bc2639f36db53ac5523432daa895ae02df10
Paul Nguyen <saviorofsparta300@gmail.com>
2017-06-22 19:11:28 -0700

    Begin Role Module

--- Megapost2/Modules/Roles.cs
+++ Megapost2/Modules/Roles.cs
@@ -3,8 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using System.Globalization;
 
 namespace Megapost2.Modules {
-    class Roles {
+
+    [Group("role")]
+    public class Roles : ModuleBase {
+
+        [Command("add")]
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        public async Task add(string s, IGuildUser[] u) {
+            var roles = Context.Guild.Roles;
+            IRole role;
+            foreach (var v in roles)
+                if (v.ToString() == s) {
+                    role = v;
+                    foreach (IGuildUser usr in u) await usr.AddRoleAsync(role);
+                } else await ReplyAsync("Role not found");
+        }
+
+        [Command("take")]
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        public async Task take(string s, IGuildUser[] u) {
+            var roles = Context.Guild.Roles;
+            IRole role;
+            foreach (var v in roles)
+                if (v.ToString() == s) {
+                    role = v;
+                    foreach (IGuildUser usr in u) await usr.RemoveRoleAsync(role);
+                } else await ReplyAsync("Role not found");
+        }
+
+        [Command("create")]
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        public async Task create(string s) {
+           await Context.Guild.CreateRoleAsync(s);
+        }
+
     }
 }
