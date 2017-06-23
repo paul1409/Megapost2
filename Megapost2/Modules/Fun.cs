1fbe5fe58794cbdb7e851baea85f87312de4ad00
Paul <paul.t.nguyen@sjsu.edu>
2017-06-22 23:39:44 -0700

    Added Google Search function

--- Megapost2/Modules/Fun.cs
+++ Megapost2/Modules/Fun.cs
@@ -5,8 +5,29 @@ using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.Commands;
+using Google.Apis.Services;
+using Google.Apis.Customsearch.v1;
 
 namespace Megapost2.Modules {
-    class Fun : ModuleBase {
+
+    public class Fun : ModuleBase<SocketCommandContext> {
+
+        [Command("google")]
+        [Remarks("Google searches the input")]
+        public async Task google([Remainder] string s) {
+            string apiKey = "AIzaSyAi4_XbS4euGFf7LJYH9jLdERF92PRELE0";
+            string cx = "006365420480420697386:a5kksrll-pc";
+            var search = new CustomsearchService(new BaseClientService.Initializer { ApiKey = apiKey });
+            var listRequest = search.Cse.List(s);
+            listRequest.Cx = cx;
+            var results = listRequest.Execute();
+            string result = "";
+            foreach (var res in results.Items) {
+                string title = string.Format("`{0}`", res.Title);
+                string link = string.Format("<{0}>", res.Link);
+                result += (title + " " + link + "\n");
+            }
+            await Context.Channel.SendMessageAsync("Showing top 10 Google results for **" +  s + "**\n"+ result);
+        }
     }
 }
