0fb756fcd27034c7d34017b51312fabc7e06417d
Paul Nguyen <saviorofsparta300@gmail.com>
2017-06-22 18:32:36 -0700

    Implement leave messages

--- Megapost2/CommandHandler.cs
+++ Megapost2/CommandHandler.cs
@@ -6,10 +6,12 @@ using System.Text;
 using System.Threading.Tasks;
 using Discord.WebSocket;
 using Discord.Commands;
+using System.IO;
 
 namespace Megapost2 {
     public class CommandHandler {
-
+        
+        
         private DiscordSocketClient client;
         private CommandService service;
 
@@ -19,6 +21,7 @@ namespace Megapost2 {
             service.AddModulesAsync(Assembly.GetEntryAssembly());
             client.MessageReceived += HandleCommandAsync;
             client.UserJoined += Join;
+            client.UserLeft += Leave;
         }
 
         private async Task HandleCommandAsync(SocketMessage s) {
@@ -33,9 +36,25 @@ namespace Megapost2 {
         }
 
         private async Task Join(SocketGuildUser u) {
+            var welcome = (Path.Combine(Directory.GetCurrentDirectory(), "Messages\\welcome.txt"));
+            var img = Path.Combine(Directory.GetCurrentDirectory(), "Newcomers.jpg");
             var channel = client.GetChannel(u.Guild.DefaultChannel.Id) as SocketTextChannel;
-            await channel.SendMessageAsync(u.Mention + " has joined the server");
+            await channel.SendFileAsync(img, string.Format(usrmsg(welcome), u.Mention));
             
         }
+
+        private async Task Leave(SocketGuildUser u) {
+            var leave = (Path.Combine(Directory.GetCurrentDirectory(), "Messages\\leave.txt"));
+            var channel = client.GetChannel(u.Guild.DefaultChannel.Id) as SocketTextChannel;
+            await channel.SendMessageAsync(string.Format(usrmsg(leave), u.ToString()));
+
+        }
+
+        private string usrmsg(string s) {
+            var lines = File.ReadAllLines(s);
+            Random r = new Random();
+            return lines[r.Next(0, lines.Length)];
+        }
+
     }
 }
