c6dc7a395b480765f2b8919bf752fbae86502594
Paul Nguyen <paul.t.nguyen@sjsu.edu>
2017-08-25 19:22:12 -0700

    Expanded Prune commands

--- Megapost2/Modules/Prune.cs
+++ Megapost2/Modules/Prune.cs
@@ -23,5 +23,48 @@ namespace Megapost2.Modules {
                 Console.WriteLine(Context.User.ToString() + " deleted " + i + " at " + DateTime.Now.ToString(@"dd\:hh\:mm"));
             }
         }
+
+        [Command("embed")]
+        public async Task embed(int i) {
+            if (i < 0) await ReplyAsync("Cannot delete a negative number of messages");
+            else if (i > 99) await ReplyAsync("Too many to delete");
+            else {
+                var m = await Context.Channel.GetMessagesAsync(i + 1).Flatten();
+                await Context.Channel.DeleteMessagesAsync(m.Where(msg => msg.Embeds.Any() || msg.Attachments.Any()));
+            }
+        }
+
+        [Command("emoji")]
+        public async Task emoji(int i) {
+            if (i < 0) await ReplyAsync("Cannot delete a negative number of messages");
+            else if (i > 99) await ReplyAsync("Too many to delete");
+            else {
+                var m = await Context.Channel.GetMessagesAsync(i + 1).Flatten();
+                await Context.Channel.DeleteMessagesAsync(m.Where(ms => ms.Tags.Any(t => t.Type == TagType.Emoji)));
+            }
+        }
+
+        [Command("mine")]
+        public async Task mine(int i) {
+            if (i < 0) await ReplyAsync("Cannot delete a negative number of messages");
+            else if (i > 99) await ReplyAsync("Too many to delete");
+            else {
+                var m = await Context.Channel.GetMessagesAsync(i + 1).Flatten();
+                await Context.Channel.DeleteMessagesAsync(m.Where(msg => msg.Author.Id == Context.User.Id));
+            }
+        }
+
+        [Command("bot")]
+        public async Task bot(int i) {
+            if (i < 0) await ReplyAsync("Cannot delete a negative number of messages");
+            else if (i > 99) await ReplyAsync("Too many to delete");
+            else {
+                var m = await Context.Channel.GetMessagesAsync(i + 1).Flatten();
+                var cmd = await Context.Channel.GetMessagesAsync(1).Flatten();
+                await Context.Channel.DeleteMessagesAsync(cmd);
+                await Context.Channel.DeleteMessagesAsync(m.Where(msg => msg.Author.IsBot));
+            }
+        }
+
     }
 }
