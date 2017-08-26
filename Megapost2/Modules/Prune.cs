94519e582523e3f9cd46b8a6fd36ff3ba736cd60
Paul Nguyen <paul.t.nguyen@sjsu.edu>
2017-08-25 20:29:53 -0700

    Reworked pruning logic

--- Megapost2/Modules/Prune.cs
+++ Megapost2/Modules/Prune.cs
@@ -14,74 +14,53 @@ namespace Megapost2.Modules {
 
         [Command]
         public async Task prune(int i) {
-            if (i < 0) await ReplyAsync("Cannot delete a negative number of messages");
-            else if (i > 99) await ReplyAsync("Too many to delete");
-            else {
-                Console.WriteLine("Deleting: " + i);
-                var m = await Context.Channel.GetMessagesAsync(i + 1).Flatten();
-                await Context.Channel.DeleteMessagesAsync(m);
-                Console.WriteLine(Context.User.ToString() + " deleted " + i + " at " + DateTime.Now.ToString(@"dd\:hh\:mm"));
-            }
+            Console.WriteLine("Deleting: " + i);
+            await PruneMsg(i, m => true);
+            Console.WriteLine(Context.User.ToString() + " deleted " + i + " at " + DateTime.Now.ToString(@"dd\:hh\:mm"));
         }
 
         [Command("embed")]
         [Summary("Deletes all messages containing embeds/attachments in the last i messages")]
         public async Task embed(int i) {
-            if (i < 0) await ReplyAsync("Cannot delete a negative number of messages");
-            else if (i > 99) await ReplyAsync("Too many to delete");
-            else {
-                var m = await Context.Channel.GetMessagesAsync(i + 1).Flatten();
-                var cmd = await Context.Channel.GetMessagesAsync(1).Flatten();
-                await Context.Channel.DeleteMessagesAsync(cmd);
-                await Context.Channel.DeleteMessagesAsync(m.Where(msg => msg.Embeds.Any() || msg.Attachments.Any()));
-            }
+            await PruneMsg(i, msg => msg.Embeds.Any() || msg.Attachments.Any());
         }
 
         [Command("emoji")]
         [Summary("Deletes all messages containing emoji in the last i messages")]
         public async Task emoji(int i) {
-            if (i < 0) await ReplyAsync("Cannot delete a negative number of messages");
-            else if (i > 99) await ReplyAsync("Too many to delete");
-            else {
-                var m = await Context.Channel.GetMessagesAsync(i + 1).Flatten();
-                await Context.Channel.DeleteMessagesAsync(m.Where(ms => ms.Tags.Any(t => t.Type == TagType.Emoji)));
-            }
+            await PruneMsg(i, ms => ms.Tags.Any(t => t.Type == TagType.Emoji));
         }
 
         [Command("mine")]
         [Summary("Deletes all messages sent by the user in the last i messages")]
         public async Task mine(int i) {
-            if (i < 0) await ReplyAsync("Cannot delete a negative number of messages");
-            else if (i > 99) await ReplyAsync("Too many to delete");
-            else {
-                var m = await Context.Channel.GetMessagesAsync(i + 1).Flatten();
-                await Context.Channel.DeleteMessagesAsync(m.Where(msg => msg.Author.Id == Context.User.Id));
-            }
+            await PruneMsg(i, msg => msg.Author.Id == Context.User.Id);
         }
 
         [Command("ping")]
         [Summary("Deletes all mentions in the last i messages")]
         public async Task ping(int i) {
-            if (i < 0) await ReplyAsync("Cannot delete a negative number of messages");
-            else if (i > 99) await ReplyAsync("Too many to delete");
-            else {
-                var m = await Context.Channel.GetMessagesAsync(i + 1).Flatten();
-                var cmd = await Context.Channel.GetMessagesAsync(1).Flatten();
-                await Context.Channel.DeleteMessagesAsync(cmd);
-                await Context.Channel.DeleteMessagesAsync(m.Where(msg => msg.MentionedUserIds.Any() || msg.MentionedRoleIds.Any()));
-            }
+            await PruneMsg(i, msg => msg.MentionedUserIds.Any() || msg.MentionedRoleIds.Any());
         }
 
         [Command("bot")]
         [Summary("Deletes all messages sent by bots in the last i messages")]
         public async Task bot(int i) {
-            if (i < 0) await ReplyAsync("Cannot delete a negative number of messages");
-            else if (i > 99) await ReplyAsync("Too many to delete");
-            else {
-                var m = await Context.Channel.GetMessagesAsync(i + 1).Flatten();
+            await PruneMsg(i, m => m.Author.IsBot);
+        }
+
+        async Task PruneMsg(int i, Func<IMessage, bool> pred = null) {
+            if (i < 0) {
+                await ReplyAsync("Cannot delete a negative number of messages");
+                return;
+            } else if (i > 99) {
+                await ReplyAsync("Too many to delete");
+                return;
+            } else {
                 var cmd = await Context.Channel.GetMessagesAsync(1).Flatten();
                 await Context.Channel.DeleteMessagesAsync(cmd);
-                await Context.Channel.DeleteMessagesAsync(m.Where(msg => msg.Author.IsBot));
+                var m = await Context.Channel.GetMessagesAsync(i).Flatten();
+                await Context.Channel.DeleteMessagesAsync(m);
             }
         }
 
