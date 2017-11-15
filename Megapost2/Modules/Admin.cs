de956ed9a9ba3ba658d9080ff875f1d2feac7f87
Paul Nguyen <paul.t.nguyen@sjsu.edu>
2017-11-14 21:47:22 -0800

    Added mute, deafen, and softban

--- Megapost2/Modules/Admin.cs
+++ Megapost2/Modules/Admin.cs
@@ -27,6 +27,17 @@ namespace Megapost2.Modules {
             }
         }
 
+        [Command("softban")]
+        [RequireUserPermission(GuildPermission.BanMembers)]
+        [Remarks("Softbans members and prunes their messages in days")]
+        public async Task Softban(int days = 7, params IGuildUser[] usr) {
+            foreach (var u in usr) {
+                ulong id = u.Id;
+                await Context.Guild.AddBanAsync(u, days);
+                await u.Guild.RemoveBanAsync(id);
+            }
+        }
+
         [Command("purge")]
         [RequireUserPermission(GuildPermission.KickMembers)]
         [Remarks("Purges users who haven't been online in specified number of days")]
@@ -41,12 +52,40 @@ namespace Megapost2.Modules {
         [Command("move")]
         [RequireUserPermission(GuildPermission.MoveMembers)]
         [Remarks("Move all user from the src channel to the dst channel")]
-        public async Task mute(IVoiceChannel src, IVoiceChannel dst) {
+        public async Task Move(IVoiceChannel src, IVoiceChannel dst) {
             var k = await src.GetUsersAsync().Flatten();
             foreach (IGuildUser u in k) {
                 await u.ModifyAsync(x => { x.Channel = new Optional<IVoiceChannel>(dst); });
                 await ReplyAsync(u.Username + ": :ok_hand:");
             }
+        }
+
+        [Command("mute")]
+        [RequireUserPermission(GuildPermission.MuteMembers)]
+        [Remarks("Mutes a member")]
+        public async Task Mute(params IGuildUser[] usr) {
+            foreach (IGuildUser u in usr) await u.ModifyAsync(user => user.Mute = true);
+        }
+
+        [Command("unmute")]
+        [RequireUserPermission(GuildPermission.MuteMembers)]
+        [Remarks("Unmutes a member")]
+        public async Task Unmute(params IGuildUser[] usr) {
+            foreach (IGuildUser u in usr) await u.ModifyAsync(user => user.Mute = false);
+        }
+
+        [Command("deafen")]
+        [RequireUserPermission(GuildPermission.MuteMembers)]
+        [Remarks("Deafens a member")]
+        public async Task Deafen(params IGuildUser[] usr) {
+            foreach (IGuildUser u in usr) await u.ModifyAsync(user => user.Deaf = true);
+        }
+
+        [Command("undeafen")]
+        [RequireUserPermission(GuildPermission.MuteMembers)]
+        [Remarks("Undefeans a member")]
+        public async Task Undeafen(params IGuildUser[] usr) {
+            foreach (IGuildUser u in usr) await u.ModifyAsync(user => user.Deaf = false);
         }
     }
 }
